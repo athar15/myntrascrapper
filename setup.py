819bc601f4f695db8b6086ac6ae3b4bd5c9ee5b7
Athar <athar.imam81@gmail.com>
2024-09-28 16:17:18 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version ='0.0.1',
+    author= 'Athar Imam',
+    author_email="athar.imam81@gmail.com",
+    packages=find_packages(),
+    install_require =[]
+)
